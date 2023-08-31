954aff10fda97aae7fca71045a18549a40320dab
Nam Dang <nathandang73@gmail.com>
2023-08-31 20:07:10 +1000

    Improve user chat layout to align with mockups design

--- fe/src/components/UserChatLayout.jsx
+++ fe/src/components/UserChatLayout.jsx
@@ -1,42 +1,90 @@
 import React from "react";
+import "../styles.css";
 
 export default function UserChatLayout() {
   return (
     <>
-      <div className="box-border w-[96.875rem] h-[61.625rem] relative m-auto">
-        {/* Footer */}
+      <div className="box-border m-auto w-[1550px] h-[986px] relative">
         <div className="absolute inset-0">
-          <div className="text-[#718096] text-center absolute left-[20px] bottom-0">
-            © 2023 AskUTS. All Rights Reserved.
+          <div className="_2023-horizon-ui-ai-template-all-rights-reserved">
+            © 2023 Horizon UI AI Template. All Rights Reserved.
           </div>
-          <div className="w-[416px] h-[24px] static">
-            <div className="text-[#718096] text-center absolute left-[1104px] bottom-0">
-              <a href="">Hompage</a>
-            </div>
-            <div className="text-[#718096] text-center absolute left-[1216px] bottom-0">
-              <a href="">License</a>
-            </div>
-            <div className="text-[#718096] text-center absolute left-[1306px] bottom-0">
-              <a href="">Terms of Use</a>
-            </div>
-            <div className="text-[#718096] text-center absolute left-[1428px] bottom-0">
-              <a href="">Privacy Policy</a>
-            </div>
+          <div className="links">
+            <div className="homepage">Homepage</div>
+            <div className="license">License</div>
+            <div className="terms-of-use">Terms of Use</div>
+            <div className="privacy-policy">Privacy Policy</div>
           </div>
         </div>
 
-        {/* Chat input */}
         <input
-          className="rounded-[45px] border-solid border-[#e2e8f0] border-[1px] w-[738px] h-[54px] absolute left-[305px] top-[825px] overflow-hidden text-[#718096] text-left p-[20px]"
-          placeholder="What are you looking for?"
+          className="absolute border border-[#e2e8f0] rounded-full w-[738px] h-[54px] overflow-hidden left-[305px] top-[825px] text-left text-[#718096] font-medium text-[14px] leading-[100%] pl-[20px]"
+          placeholder="Send a message"
         ></input>
 
-        {/* Submit button */}
-        <button className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-[45px] flex flex-row gap-0 items-center justify-center w-[192px] h-[54px] absolute left-[1053px] top-[825px] shadow-2xl overflow-hidden">
-          <div className="text-[#ffffff] text-left relative w-[50px] h-[20px] font-semibold text-base leading-[16px]">
+        <button
+          className="absolute flex items-center justify-center w-[192px] h-[54px] rounded-full opacity-20 overflow-hidden left-[1053px] top-[825px] bg-gradient-to-r from-purple-500 to-indigo-500 shadow-lg"
+          disabled
+        >
+          <div className="text-white text-left font-semibold text-[14px] leading-[16px] relative w-[50px] h-5">
             Submit
           </div>
         </button>
+
+        <div className="text-center font-medium text-[12px] leading-[100%] absolute left-1/2 transform -translate-x-1/2 top-[899px] w-[715px]">
+          <span>
+            <span className="text-[#718096] font-medium text-[12px] leading-[100%]">
+              Free Research Preview. ChatGPT may produce inaccurate information
+              about people, places, or facts.{" "}
+            </span>
+            <span className="text-[#120f43] font-medium text-[12px] leading-[100%] underline">
+              ChatGPT May 12 Version
+            </span>
+          </span>
+        </div>
+
+        <div className="api-key-card">
+          <div className="save-button">
+            <div className="save">Save</div>
+          </div>
+          <div className="input2">
+            <div className="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">
+              sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+            </div>
+          </div>
+          <div className="title">Enter your OpenAI API Key</div>
+          <div className="body">
+            You need an OpenAI API Key to use Horizon Chat UI.
+            <br />
+            Your API Key is stored locally on your browser and never sent
+            anywhere else.
+          </div>
+          <div className="the-app-will-connect-to-open-ai-api-server-to-check-if-your-api-key-is-working-properly">
+            *The app will connect to OpenAI API server to check if your API Key
+            is working properly.
+          </div>
+          <div className="get-your-api-key-from-open-ai-dashboard">
+            Get your API key from Open AI Dashboard
+          </div>
+          <div className="your-api-key-is-not-working">
+            <div className="your-api-key-is-not-working2">
+              Your API Key is not working?
+            </div>
+            <svg
+              className="vector3"
+              width="10"
+              height="6"
+              viewBox="0 0 10 6"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.06037 0.24375C8.73537 -0.08125 8.21037 -0.0812501 7.88537 0.24375L4.65204 3.47708L1.41871 0.24375C1.09371 -0.0812504 0.568707 -0.0812504 0.243707 0.24375C-0.0812931 0.56875 -0.0812932 1.09375 0.243707 1.41875L4.06871 5.24375C4.39371 5.56875 4.91871 5.56875 5.24371 5.24375L9.06871 1.41875C9.38537 1.10208 9.38537 0.56875 9.06037 0.24375Z"
+                fill="#1B2559"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </>
   );
