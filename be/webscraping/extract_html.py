424ad373427a59c2d02df559aa983570b8e8db37
MelisaTims <MelisaTimsina@gmail.com>
2023-09-05 19:23:55 +1000

    Data can be extracted from a dropdown ( Collapsible section)

--- be/webscraping/extract_html.py
+++ be/webscraping/extract_html.py
@@ -10,7 +10,8 @@ domain = sitemap[0]
 sitemap = [
     "https://www.uts.edu.au",
     "https://www.uts.edu.au/current-students/managing-your-course/important-dates/census-date", 
-    "https://www.uts.edu.au/current-students/managing-your-course/important-dates/last-day-enrol"]
+    "https://www.uts.edu.au/current-students/managing-your-course/important-dates/last-day-enrol",
+    "https://www.uts.edu.au/current-students/managing-your-course/your-enrolment/enrolling-subjects"]
 
 output_folder = "uts_website_extracted"
 
@@ -24,14 +25,13 @@ def extract_text_from_url(url, output_filename):
 
     # Find all <p> tags and extract their text content
     p_tags = soup.find_all('p')
-    p_tags = p_tags[:-4] #remove aknowledgement of country  removing footers * find out how* - look into this
+    p_tags = p_tags[:-4] #remove aknowledgement of country
 
     paragraph_text = [p.get_text() for p in p_tags ]
 
     text = '\n'.join(paragraph_text)  # Join paragraphs with newline
 
      # Find all <table> tags and extract their text content
-
     table_tag = soup.find_all('table')
     for table_index, table in enumerate(table_tag):
         table_text = []
@@ -43,9 +43,23 @@ def extract_text_from_url(url, output_filename):
             table_text.append('\t'.join(row_content))
         text += '\nTable {}:'.format(table_index + 1) + '\n' + '\n'.join(table_text)
 
+
+
+# Extract from dropdowns (Collapsible sections)
+    dropdown_content = soup.find_all('section', class_='collapsible')
+    for index, dropdown in enumerate(dropdown_content):
+        dropdown_title = dropdown.find('h3', class_='js-collapsible collapsible__title')
+        dropdown_content = dropdown.find('div', class_='collapsible__content')
+        
+        if dropdown_title and dropdown_content:
+            title_txt = dropdown_title.get_text()
+            content_txt = dropdown_content.get_text()
+            
+            text += f'\nCollapsible Section {index + 1}: {title_txt}\n{content_txt}'
+
+    # Return the extracted text
     return text
 
-# Extract from dropdowns, ( header question -> drop down with answer)  choose 100 most used websites or choose by faculty
 
 # Create a CSV file
 with open('web_scraped_data.csv', 'w', newline='', encoding="utf-8") as csv_file:
