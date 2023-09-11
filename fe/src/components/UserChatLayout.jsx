c25e71353f17f1a842f518af7964af8a7510b013
Nam Dang <nathandang73@gmail.com>
2023-09-12 01:59:52 +1000

    Make input and disclaimer use px instead of %

--- fe/src/components/UserChatLayout.jsx
+++ fe/src/components/UserChatLayout.jsx
@@ -56,13 +56,13 @@ export default function UserChatLayout() {
 
           {/* Chat input */}
           <input
-            className="absolute border border-[#e2e8f0] rounded-full w-[80%] lg:w-[40%] h-[54px] overflow-hidden lg:left-1/2 lg:transform lg:-translate-x-1/2 bottom-[220px] lg:bottom-[140px] text-left text-[#718096] font-medium text-[14px] leading-[100%] pl-[20px]"
+            className="absolute border border-[#e2e8f0] rounded-full w-[80%] lg:w-[700px] h-[54px] overflow-hidden lg:left-1/2 lg:transform lg:-translate-x-1/2 bottom-[220px] lg:bottom-[140px] text-left text-[#718096] font-medium text-[14px] leading-[100%] pl-[20px]"
             placeholder="Send a message"
           ></input>
 
           {/* Submit button */}
           <button
-            className="absolute flex items-center justify-center w-[100px] h-[54px] rounded-full opacity-20 overflow-hidden lg:left-1/2 lg:transform lg:-translate-x-[-330px] bottom-[100px] lg:bottom-[140px] bg-gradient-to-r from-blue-600 to-[#0f4beb] shadow-lg"
+            className="absolute flex items-center justify-center w-[100px] h-[54px] rounded-full opacity-20 overflow-hidden lg:left-1/2 lg:transform lg:-translate-x-[-370px] bottom-[100px] lg:bottom-[140px] bg-gradient-to-r from-blue-600 to-[#0f4beb] shadow-lg"
             disabled
           >
             <div className="text-white text-left font-semibold text-[14px] leading-[16px] relative w-[50px] h-5">
@@ -71,7 +71,7 @@ export default function UserChatLayout() {
           </button>
 
           {/* Disclaimer */}
-          <div className="text-center font-medium text-[12px] leading-[100%] absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 bottom-[170px] lg:bottom-[100px] w-[80%] lg:w-[30%]">
+          <div className="text-center font-medium text-[12px] leading-[100%] absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 bottom-[170px] lg:bottom-[100px] w-[80%] lg:w-[700px]">
             <span>
               <span className="text-[#718096] font-medium text-[12px] leading-[100%]">
                 Free Research Preview. ChatGPT may produce inaccurate
