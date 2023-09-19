3a2a3100e8ffade9ccad497e9405663ff3802458
MelisaTims <MelisaTimsina@gmail.com>
2023-09-20 09:17:38 +1000

    unit test for extracting text from url

--- be/webscraping/unit_test_webscraping.py
+++ be/webscraping/unit_test_webscraping.py
@@ -1,4 +1,3 @@
-# Import the required dependencies, including extract_html import
 import unittest
 from unittest.mock import patch, mock_open
 from extract_html import extract_text_from_url, sort_urls_by_keyphrase
@@ -6,23 +5,31 @@ from extract_html import extract_text_from_url, sort_urls_by_keyphrase
 # Test class needs to be defined
 # URL content extraction (test case)
 class TestAllFunctionalities(unittest.TestCase):
-    def unittest_extract_text_from_url(self, mock_open_file, mock_os_makedirs, mock_requests_get):
-    
-        # Insert list of sample URL by mapping through the URL
-        # mock response with sample html
-        # extract_text_from_url function needs to be called along with the URL + filename
-        output_folder = "unittest_output"
-        output_filename = "unittest_output.txt"
-        extracted_text = extract_text_from_url(url, output_filename)
-        # Check if the extracted content matches the expected content
-        #Compare betweeen 'extracted_text' and 'html_content'
+    @patch('requests.get')
+    @patch('os.makedirs')
+    @patch('builtins.open', new_callable=mock_open)
+    def test_extract_text_from_url(self, mock_open, mock_makedirs, mock_requests_get):
+        url_html_mapping = {
+            "https://www.uts.edu.au/about/faculty-science/biosciences-and-proteomics-technologies/courses-and-training": "<html><body><p>Airline Pilot Dominique Cotte successfully transitioned her career during COVID-19 to the cyber security industry by becoming trained through our part-time cyber security program.</p></body></html>"
+        }
 
-    # Filtering URL test case by keyphrase
-    def unittest_sort_urls_by_keyphrase(self):
-         #list of URL 
-        # sort_urls_by_keyphrase function should be called
-        sorted_urls = sort_urls_by_keyphrase(urls, "engineering")
-        # check if the 'sorted_urls' mtaches the expected result
+        for url, html_content in url_html_mapping.items():
+            mock_response = mock_requests_get.return_value
+            mock_response.content = html_content
+            output_folder = "unittest_folder"
+            output_fileName = "unittest_folder.txt"
+            extracted_text = extract_text_from_url(url, output_folder, output_fileName)
+            expected_text = f"Airline Pilot Dominique Cotte successfully transitioned her career during COVID-19 to the cyber security industry by becoming trained through our part-time cyber security program. {list(url_html_mapping.keys()).index(url) + 1}"
+
+
+    #  # Filtering URL test case by keyphrase
+    # def unittest_sort_urls_by_keyphrase(self):
+    #      #list of URL 
+    #     # sort_urls_by_keyphrase function should be called
+    #     sorted_urls = sort_urls_by_keyphrase(urls, "engineering")
+    #     # check if the 'sorted_urls' mtaches the expected result
 
 if __name__ == "__main__":
     unittest.main()
+
+
