cac65ff376bfe49b18f47ce3b4079ee7d20f445c
MelisaTims <MelisaTimsina@gmail.com>
2023-10-03 02:01:39 +1100

    Unit test for get paragraph for text function and code clean up

--- be/webscraping/unit_test_webscraping.py
+++ be/webscraping/unit_test_webscraping.py
@@ -1,33 +1,18 @@
 import unittest
-from unittest.mock import patch, mock_open
-from extract_html import extract_text_from_url, sort_urls_by_keyphrase
+from bs4 import BeautifulSoup 
+from extract_html import get_paragraphs_from_text
 
+class TestExtractHtml(unittest.TestCase):
 
-# Test class needs to be defined
-# URL content extraction (test case)
-class TestAllFunctionalities(unittest.TestCase):
-    @patch("requests.get")
-    @patch("os.makedirs")
-    @patch("builtins.open", new_callable=mock_open)
-    def test_extract_text_from_url(self, mock_open, mock_makedirs, mock_requests_get):
-        url_html_mapping = {
-            "https://www.uts.edu.au/about/faculty-science/biosciences-and-proteomics-technologies/courses-and-training": "<html><body><p>Airline Pilot Dominique Cotte successfully transitioned her career during COVID-19 to the cyber security industry by becoming trained through our part-time cyber security program.</p></body></html>"
-        }
+    def test_get_paragraphs_from_text(self):
+        html_content = '''<p class="MsoNormal"><span lang="EN-US" xml:lang="EN-US">The University of Technology Sydney (UTS) has launched the Robotics Institute, a cutting-edge research centre at the forefront of robotics research and innovation, led by globally recognised robotics and mechatronics expert Professor Sarath Kodagoda.&nbsp;&nbsp;</span></p>'''
+        soup = BeautifulSoup(html_content, "html.parser")
 
-        for url, html_content in url_html_mapping.items():
-            mock_response = mock_requests_get.return_value
-            mock_response.content = html_content
-            output_folder = "unittest_folder"
-            output_fileName = "unittest_folder.txt"
-            extracted_text = extract_text_from_url(url, output_folder, output_fileName)
-            expected_text = f"Airline Pilot Dominique Cotte successfully transitioned her career during COVID-19 to the cyber security industry by becoming trained through our part-time cyber security program. {list(url_html_mapping.keys()).index(url) + 1}"
+        result = get_paragraphs_from_text(soup)
 
-    #  Filtering URL test case by keyphrase
-    # def unittest_sort_urls_by_keyphrase(self):
-    #     # list of URL
-    #     # sort_urls_by_keyphrase function should be called
-    #     sorted_urls = sort_urls_by_keyphrase(urls, "engineering")
-    #     # check if the 'sorted_urls' mtaches the expected result
+        expected_result = "The University of Technology Sydney (UTS) has launched the Robotics Institute, a cutting-edge research centre at the forefront of robotics research and innovation, led by globally recognised robotics and mechatronics expert Professor Sarath Kodagoda."
+
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == "__main__":
