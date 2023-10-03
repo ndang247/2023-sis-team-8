78a880423c4382e7c64f435ec9680bb62573370b
Yumna Zafar <yumna.zafar2@gmail.com>
2023-10-03 23:06:33 +1100

    cleanup icon fix

--- fe/src/components/Sidebar.jsx
+++ fe/src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import {
-  UploadOutlined,
+  ContainerOutlined,
   UserOutlined,
-  VideoCameraOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  SearchOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Input } from "antd";
 const { Sider } = Layout;
+const { SubMenu } = Menu;
 
 export const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -23,23 +26,38 @@ export const Sidebar = () => {
         console.log(collapsed, type);
         setCollapsed(collapsed);
       }}
+      style={{ backgroundColor: "#123286" }}
     >
       <div className="demo-logo-vertical" />
+      <Input
+        prefix={<SearchOutlined style={{ color: 'rgba(118, 118, 118, 0.5)' }} />} 
+        placeholder="Search"
+        style={{ margin: '16px', width: '80%', color: 'rgba(118, 118, 118, 0.5)', alignSelf: 'center' }}
+      />
       <Menu
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["4"]}
-        items={[
-          UserOutlined,
-          VideoCameraOutlined,
-          UploadOutlined,
-          UserOutlined,
-        ].map((icon, index) => ({
-          key: String(index + 1),
-          icon: React.createElement(icon),
-          label: `nav ${index + 1}`,
-        }))}
-      />
+        style={{ backgroundColor: "#123286", color: 'white' }}
+      >
+        <SubMenu
+          key="sub1"
+          title={
+            <span>
+              <ContainerOutlined />
+              <span>My Questions</span>
+            </span>
+          }
+        >
+          <Menu.Item key="1">Option 1</Menu.Item>
+          <Menu.Item key="2">Option 2</Menu.Item>
+          <Menu.Item key="3">Option 3</Menu.Item>
+        </SubMenu>
+        <Menu.Item key="2" icon={<UserOutlined />}>
+          Contact Us
+        </Menu.Item>
+      </Menu>
+      <div style={{ textAlign: 'center', color: 'white', paddingTop: '25px' }}>To speak to someone, please call 1300 ASK UTS</div>
     </Sider>
   );
 };
