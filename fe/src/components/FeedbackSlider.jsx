0239dc12a6dc89676d764d16fecc6cb84af0ee36
Yumna Zafar <yumna.zafar2@gmail.com>
2023-10-11 09:14:50 +1100

    rewrite code

--- fe/src/components/FeedbackSlider.jsx
+++ fe/src/components/FeedbackSlider.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
 import { Slider } from 'antd';
 
 const FeedbackSlider = ({ max, min }) => {
   const [value, setValue] = useState(0);
 
-  const mid = Number(((max - min) / 2).toFixed(5));
-  const preColorCls = value >= mid ? '' : 'icon-wrapper-active';
-  const nextColorCls = value >= mid ? 'icon-wrapper-active' : '';
+  const mid = useMemo(() => Number(((max - min) / 2).toFixed(5)), [max, min]);
+  const isValueGreaterThanMid = value >= mid;
 
   return (
     <div className="icon-wrapper">
-      <FrownOutlined className={preColorCls} />
+      <FrownOutlined className={isValueGreaterThanMid ? '' : 'icon-wrapper-active'} />
       <Slider max={max} min={min} onChange={setValue} value={value} />
-      <SmileOutlined className={nextColorCls} />
+      <SmileOutlined className={isValueGreaterThanMid ? 'icon-wrapper-active' : ''} />
     </div>
   );
 };
