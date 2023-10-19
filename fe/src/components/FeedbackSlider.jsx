2859854579c78f63e3c020fcb606ad9fbe248959
Yumna Zafar <yumna.zafar2@gmail.com>
2023-10-19 14:55:13 +1100

    fix

--- fe/src/components/FeedbackSlider.jsx
+++ fe/src/components/FeedbackSlider.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import { LikeOutlined, DislikeOutlined } from '@ant-design/icons';
-import { Slider } from 'antd';
+import { Slider, message } from 'antd';
 
 const IconSlider = (props) => {
   const { max, min } = props;
   const [value, setValue] = useState(0);
+  const [interacted, setInteracted] = useState(false); // state variable to handle interaction
+
   const mid = Number(((max - min) / 2).toFixed(5));
   const preColorCls = value >= mid ? '' : 'icon-wrapper-active';
   const nextColorCls = value >= mid ? 'icon-wrapper-active' : '';
-  
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+    setInteracted(true); // handle the change
+  };
+
+  if (interacted) {
+    message.info('Thankyou for your feedback'); //message being outputted
+  }
+
   return (
     <div className="icon-wrapper">
       <LikeOutlined className={preColorCls} />
-      <Slider {...props} onChange={setValue} value={value} />
+      <Slider {...props} onChange={handleChange} value={value} />
       <DislikeOutlined className={nextColorCls} />
     </div>
   );
