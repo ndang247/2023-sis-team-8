292a869c54cbdc4e84942dc38b9379e38cec9052
Andy Le <andy.le314@gmail.com>
2023-10-24 19:22:49 +1100

    Adding more test cases

--- be/test_main.py
+++ be/test_main.py
@@ -18,7 +18,6 @@ def test_chat_valid_message():
     assert response.json()["message"]["text"] == "Hello AskUTS chatbot"
     assert response.json()["message"]["timeStamp"] == "1970-01-01T00:02:03Z"
 
-
 def test_chat_remove_whitespace():
     response = client.post(
         "/chat/", json={"text": "      no white space!    ", "timeStamp": 444}
@@ -49,3 +48,40 @@ def test_chat_missing_timeStamp():
         == "Value error, Time stamp was not provided!"
     )
 
+def test_embedded_search_valid():
+    url = "/embedded_search/?prompt=I%27m%20looking%20for%20financial%20support%20scholarships?"
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.json()["matches"]) != 0
+
+def test_chat_read_all_valid():
+    response = client.get("/chat_read")
+    assert response.status_code == 200
+    assert len(response.json()) != 0
+
+def test_chat_read_invalid_id():
+    url = "/chat_read/?message_id=123"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert (
+        response.json()["detail"]
+        == "'123' is not a valid id, it must be a 12-byte input or a 24-character hex string"
+    ) 
+
+def test_chat_read_existent_id():
+    url = "/chat_read/?message_id=65377d533d07ecdcb34b16d0"
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.json()["message"]["text"] == "Hello! What engineering courses does UTS offer?"
+    assert len(response.json()["similarity"]) != 0
+    assert len(response.json()["answerURL"]) != 0
+
+
+def test_chat_read_non_existent_id():
+    url = "/chat_read/?message_id=63080c44cf54ac1d6409a1be"
+    response = client.get(url)
+    assert response.status_code == 404
+    assert (
+        response.json()["detail"]
+        == "'63080c44cf54ac1d6409a1be' does not exist!"
+    ) 
