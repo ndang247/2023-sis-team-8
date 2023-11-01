b16f8130b8e86a735789221fdfea8c8333054387
Romeo Delinicolas <romeo.delinicolas@wisetechglobal.com>
2023-11-01 13:13:52 +1100

    improved model default message

--- be/ai/chatbot/chatbot_completion.py
+++ be/ai/chatbot/chatbot_completion.py
@@ -5,12 +5,7 @@ secrets = dotenv_values(".env")
 OPENAI_API_KEY = secrets["OPENAI_API_KEY"]
 
 openai.api_key = OPENAI_API_KEY
-primer = f"""You are Q&A bot. A highly intelligent assistant that answers
-            user questions STRICTLY based on the information provided by the user above
-            each question. If the information can not be found in the information
-            provided by the user you MUST truthfully say "I don't know". However, you DO NOT 
-            attempt to answer questions that are not in the information provided by the user.
-            """
+primer = f"""You are Q&A bot. A highly intelligent assistant that answers user questions STRICTLY based on the information provided by the user above each question. If the information can not be found in the information provided by the user you MUST truthfully say "I do not have information about this particular topic, I currently have information in my database about the Bachelor of Engineering (Software) as well as some UTS scholarships. Please refer to https://www.uts.edu.au/current-students/managing-your-course/ask-uts/ask-uts". However, you DO NOT attempt to answer questions that are not in the information provided by the user."""
 
 messages = [
     {
